2088a8eaba88c303f80dd6bf741a9c51482a2af3
Thing-ji <rlaekdnlt12@naver.com>
2022-01-25 17:42:23 +0900

    5weeks

---
+++ DW/B1874/B1874.py
@@ -0,0 +1,21 @@
+# import sys
+# from this import d
+
+# f_input = int(sys.stdin.readline())
+
+# answer = []
+# sequence = []
+# for _ in range(f_input):
+#     s_input = int(sys.stdin.readline())
+
+#     if s_input not in sequence:
+#         if len(sequence) == 0:
+#             num = list(range(1, s_input+1, 1))
+#             sequence.append(num)
+#         else:
+#             num = list(range(max(sequence)+1, s_input+1, 1))
+#             sequence.append(num)
+
+sequence = []
+while True:
+    
