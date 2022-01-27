3580d907cd9758bcf7e5572808a8352bae180e92
leejuhanKr <juhan96@gmail.com>
2022-01-27 23:54:19 +0900

    문제 업데이트

--- JH/2644_촌수계산/1.py
+++ JH/2644_촌수계산/1.py
@@ -1,6 +1,6 @@
 #%%
 n_max = 9
-start, end = 7, 3
+start, end = 7-1, 3-1
 n_line = 7
 lines= [
     (1, 2),
@@ -12,14 +12,39 @@ lines= [
     (4, 6),
     ]
 
+from collections import deque
+from sys import stdin
+n_max = int(input())
+start, end = map(lambda x: int(x)-1, input().split())
+n_line = int(input())
+lines = [tuple(map(int,stdin.readline().split())) 
+    for _ in range(n_line)]
+
 map_ = [[0]*n_max for _ in range(n_max)]
 for line in lines:
     r,c = line[0]-1, line[1]-1
-    map_[r][c] = 1
-    map_[c][r] = 1
+    map_[r][c] = -1
+    map_[c][r] = -1
 
-print(*map_, sep="\n")
-# %% bfs
+visited=deque()
+visited.append(start)
+map_[start][start]=0
+
 def bfs():
-    visited = []
-    
+    while visited:
+        tmp = visited.popleft()
+        tmp_ans = map_[tmp][tmp]
+        if tmp == end:
+            return tmp_ans
+
+        for i,v in enumerate(map_[tmp]):
+            if v == -1:
+                visited.append(i)
+                map_[tmp][i] = 0
+                map_[i][tmp] = 0
+                map_[i][i] = tmp_ans+1
+    return -1
+
+print(bfs())
+
+# %%
