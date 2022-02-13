2a9f6a5ec7bce405aba4097268b4c4ad5e1564ac
leejuhanKr <juhan96@gmail.com>
2022-02-13 23:28:48 +0900

    문제 업데이트

---
+++ JH/2206_벽 부수고 이동하기/1 - 복사본 - 복사본 (2).py
@@ -0,0 +1,18 @@
+#%%
+from sys import stdin
+n,m = 6,4
+map_ = [
+    [0,1,0,0,],
+    [1,1,1,0,],
+    [1,0,0,0,],
+    [0,0,0,0,],
+    [0,1,1,1,],
+    [0,0,0,0,],
+    ]
+from collections import deque
+
+
+def bfs(map_,start,end):
+    
+
+bfs(map_,(0,0),(n-1,m-1))
